6791ef27b500b1ae13f89146b859840e3bd75fff
billyovo <30436689+billyovo@users.noreply.github.com>
2022-04-18 15:44:24 +0800

    change port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const crypto = require("./helpers/crypto.js");
 const auth = require("./middleware/auth.js");
 
 const app = express();
-const port = 3000
+const port = 80;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
