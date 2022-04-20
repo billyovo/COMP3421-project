b04f1b56c0065bf226cb552acb22b41960275302
billyovo <30436689+billyovo@users.noreply.github.com>
2022-04-20 20:55:44 +0800

    cors 4.0

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const crypto = require("../helpers/crypto.js");
 
 function auth(req,res,next){
-    const access_token = req.cookies.access_token;
+    const access_token = req.cookies.access_token || req.cookies.refresh_token;
     
     const verify_token = crypto.verifyToken(access_token);
     if(verify_token){
